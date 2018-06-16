b1a007468015142a4b8edcf42748fef19472d27d
Nino Page <Nino.Page@ecofog.gf>
2018-06-16 06:03:45 -0300

    master thesis first commit4

---
+++ plots/notes.R
@@ -0,0 +1,30 @@
+
+For example, rainforests in the Amazon
+sequester carbon from the global atmosphere, regulate the
+water balance and flow of the entire Amazon River system,
+influence the patterns of climate and air chemistry over
+much of the continent,
+Ina dditiont op rovidinga griculturaln d timberc ommodities
+these landscapes may also sequester carbon, regulate fresh
+water and river flows, modulate regional patterns of climate,
+anda melioratein fectiouds iseases
+
+
+OK lets go[@Guariguata2000]
+
+@Andrade2017 proposed average emissions 19.9 MgC/ha for logging and 46.0 MgC/ha
+forest degradation from logging fire is a major threat
+
+@piponiot2016 We show that net aboveground C recovery over 10 years is higher in the Guiana Shield and in the west (21 ±3 Mg C ha-1) than in the south (12 ±3 Mg C ha-1)  http://dx.doi.org/10.7554/eLife.21394.002
+Camila : 
+  
+  in the Brazilian Amazon alone, about 2 Mha yr−1 were logged in 1999–2002 (Asner et al., 2005)
+
+
+
+The extent of selective logging in the Brasilian Amazon was equivalent to annual deforestation in the same period, and resulted in C emissions of 90 Tg C yr-1 (Huang and Asner, 2010) 
+which increased anthropogenic C emissions by almost 25% over deforestation alone (Asner et al., 2005)
+
+Although annual deforestation rates in the Brazilian Amazon have declined by 80% since 2004 (Hansen et al 2014, INPE 2015) in @rappaport
+
+Even if the Carbon emissions involved are high ,uncertainties in carbon accounting (@Rappaport2018, PIPONIOT). The REDD+ accounting framework is supposed(@Mertz2012)Growing attention is paid to forest degradation, althought high uncertaineties making its integration in REDD+, 
